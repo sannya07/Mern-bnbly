c38e951761f2a1bcf5a16c8fe307edeb56772574
sannya07 <sanyakansal07@gmail.com>
2025-08-08 11:52:28 +0530

    logout route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -61,10 +61,20 @@ export const login=async(req,res)=>{
             sameSite: "strict",
             maxAge: 7*24*60*60*1000
         })
-        return res.status(201).json(user)
+        return res.status(200).json(user)
     } catch (error) {
         console.log(error);
         return res.status(500).json({message:"Sign in error"})
         
+    }
+}
+
+export const logout=async(req,res)=>{
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({message:"Logout successfully"})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message:"Logout error"})
     }
 }
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { login, signUp } from '../controllers/auth.controller.js';
+import { login, logout, signUp } from '../controllers/auth.controller.js';
 
 const authRouter=express.Router();
 
 authRouter.post("/signup",signUp);
 authRouter.post("/login",login);
+authRouter.post('/logout',logout)
 
 export default authRouter;
