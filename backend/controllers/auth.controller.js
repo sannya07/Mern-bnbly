fb20bcd32e8c01ff88460dcba8a6df91c01f040c
sannya07 <sanyakansal07@gmail.com>
2025-08-08 11:47:13 +0530

    login logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,5 @@
 import User from "../models/User.model.js";
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import generateToken from "../config/token.js";
 
 
@@ -48,8 +47,24 @@ export const login=async(req,res)=>{
         if(!user){
             return res.status(400).json({message:"User is not exist"})
         }
-        
+        let isMatch= await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message:"incorrect password!"
+            })
+        }
+
+        let token=await generateToken(user._id)
+        res.cookie("token",token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENVIRONMENT="production",
+            sameSite: "strict",
+            maxAge: 7*24*60*60*1000
+        })
+        return res.status(201).json(user)
     } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"Sign in error"})
         
     }
 }
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signUp } from '../controllers/auth.controller.js';
+import { login, signUp } from '../controllers/auth.controller.js';
 
 const authRouter=express.Router();
 
 authRouter.post("/signup",signUp);
+authRouter.post("/login",login);
 
 export default authRouter;
